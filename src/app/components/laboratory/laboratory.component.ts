485235fb14489d6f27f6c35b03460ea4abd66ef3
shotttik <shota.akhlouri@gmail.com>
2023-03-01 02:12:55 +0400

    added laboratory component

---
+++ src/app/components/laboratory/laboratory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laboratory',
+  templateUrl: './laboratory.component.html',
+  styleUrls: ['./laboratory.component.css']
+})
+export class LaboratoryComponent {
+
+}
